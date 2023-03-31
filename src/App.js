fbe2b250229c0c571e359e9048a48fe59db471b5
Priya28062002 <Priyaanand0628@gmail.com>
2023-03-31 15:15:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> good morning</h1>
+        <button onClick={window['lek']}>press button</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
